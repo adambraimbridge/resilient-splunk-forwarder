2f9eb6a68b220eb0d471dac2a9b248b8ae87a945
Tamas Molnar <tamas.molnar@ft.com>
2018-09-12 17:28:06 +0300

    Try summary with customer observer to measure latency in microseconds

--- processor.go
+++ processor.go
@@ -44,16 +44,16 @@ var (
 )
 
 func NewLogProcessor(forwarder Forwarder, cache Cache, config appConfig) LogProcessor {
-	ql := prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Namespace: "upp",
-			Subsystem: "splunk_forwarder",
-			Name:      "queue_latency",
-			Help:      "Post queue latency",
+	ql := prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  "upp",
+			Subsystem:  "splunk_forwarder",
+			Name:       "queue_latency",
+			Help:       "Post queue latency",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
 		},
 		[]string{"environment"},
 	)
-
 	prometheus.Register(ql)
 	queueLatency = ql.With(prometheus.Labels{"environment": config.env})
 	return &logProcessor{forwarder: forwarder, cache: cache, wg: sync.WaitGroup{}, chanBuffer: config.chanBuffer, workers: config.workers}
@@ -129,12 +129,15 @@ func (logProcessor *logProcessor) Start() {
 					time.Sleep(sleepDuration)
 				}
 				t := metrics.GetOrRegisterTimer("post.queue.latency", metrics.DefaultRegistry)
-				prometeusTimer := prometheus.NewTimer(queueLatency)
+				prometheusTimer := prometheus.NewTimer(prometheus.ObserverFunc(func(v float64) {
+					us := v * 1000000 // make microseconds
+					queueLatency.Observe(us)
+				}))
 				t.Time(func() {
 					log.Printf("Sending document to channel")
 					logProcessor.outChan <- entry
 				})
-				prometeusTimer.ObserveDuration()
+				prometheusTimer.ObserveDuration()
 			}
 
 			// don't overwhelm S3 when it's empty
